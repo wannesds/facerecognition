9835c067fc6f8abf503e57a069b17febddc1ddad
wannesds <wannesdieltiens@gmail.com>
2021-01-22 19:31:22 +0100

    initialize

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './components/Vars.scss';
 
 ReactDOM.render(
   <React.StrictMode>
