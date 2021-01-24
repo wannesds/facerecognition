309c9c95de9d8ae9f58a5b5f61d78dd0cb36c69e
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-01-24 18:44:45 +0100

    sync

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-
+import React, { useState } from 'react';
 import './App.scss';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const onInputChange = (event) => {
+      console.log(event.target.value);
+  }
+
+  const onBtnSubmit = () => {
+    console.log('click');
+  }
+  
   return (
     <div className="App">
       <Navigation />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={onInputChange} onBtnSubmit={onBtnSubmit}/>
       {/* <FaceRecognition/>} */}
     </div>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ImageLinkForm.scss';
+//.predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
 
+function ImageLinkForm({ onInputChange, onBtnSubmit }) {
+    
 
- const ImageLinkForm = () => {
      return(
         <div className="form-box">
             <p>
                 {'Detect faces in your pictures.'}
             </p>
             <div className="form-input">
-                <input type='text'/>
-                <button>Scan</button>
+                <input type='text' onChange={onInputChange}/>
+                <button onClick={onBtnSubmit}>Scan</button>
             </div>
         </div>
      );
