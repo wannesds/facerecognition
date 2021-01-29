1463716a80cbae943f5811811708d52fb2496ead
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-01-29 15:22:22 +0100

    sync

--- src/App.js
+++ src/App.js
@@ -24,19 +24,36 @@ function App() {
   const [box, setBox] = useState({});
   const [route, setRoute] = useState('signin');
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  });
 
   const [data, setData] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      fetch('http://localhost:3000')
-      .then(response => response.json())
-      .then(res => setData(res))
-    }
-    fetchData()
-  },[])
+  // useEffect(() => {
+  //   const fetchData = () => {
+  //     fetch('http://localhost:3000')
+  //     .then(response => response.json())
+  //     .then(res => setData(res))
+  //   }
+  //   fetchData()
+  // },[])
 
-  console.log('data after useeffect', data);
+  //console.log('data after useeffect', data);
+
+  const loadUser = (data) => {
+    setUser({
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    });
+  }
 
   const calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -101,7 +118,7 @@ function App() {
         :  (
           route === 'signin'
             ? <Signin onRouteChange={onRouteChange}/> 
-            : <Register onRouteChange={onRouteChange}/>
+            : <Register onRouteChange={onRouteChange} loadUser={loadUser}/>
         )
           
         
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Signin/Signin.scss'; //bcs same styles
 
 
- const Register = ({onRouteChange}) => {
+function Register({onRouteChange, loadUser}) {
+
+    const [regPass, setRegPass] = useState('');
+    const [regEmail, setRegEmail] = useState('');
+    const [regName, setRegName] = useState('');
+
+    const onEmailChange = (event) => {
+        setRegEmail(event.target.value);
+    }
+
+    const onPassChange = (event) => {
+        setRegPass(event.target.value);
+    }
+
+    const onNameChange = (event) => {
+        setRegName(event.target.value);
+    }
+
+    const onSubmitReg = () =>{
+        fetch('http://localhost:3000/register', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: regEmail,
+                password: regPass,
+                name: regName
+            })
+        })
+        .then(response => response.json())
+        .then(user => { 
+            if(user) {
+                loadUser(user);
+                onRouteChange('home');
+            }
+        })   
+    }
+
+
      return(
         <div className="signin-box">
             <div className="form">
                 <fieldset id="register">
                     <legend>Register</legend>
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="name"></input>
+                    <input type="text" name="name" onChange={onNameChange}></input>
                     <label htmlFor="email" >Email</label>
-                    <input type="email" name="email"></input>
+                    <input type="email" name="email" onChange={onEmailChange}></input>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password"></input>
-                    <input className="button" onClick={() => onRouteChange('home')} type="submit" value="Register"></input>
+                    <input type="password" name="password" onChange={onPassChange}></input>
+                    <input className="button" onClick={onSubmitReg} type="submit" value="Register"></input>
                 </fieldset>
             </div>
         </div>
