91bbc306565f8729df97858f96c984657d39af1d
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-02-06 10:32:51 +0100

    fix

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,10 +23,11 @@ function Register({onRouteChange, loadUser}) {
     const onSubmitReg = () => {
         fetch('https://git.heroku.com/frozen-shore-44702.git/register', {
             method: 'post',
-            mode: 'no-cors',
+            
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin': '*',
+                'Connection': 'application/x-www-form-urlencoded'
             },
             body: JSON.stringify({
                 email: regEmail,
