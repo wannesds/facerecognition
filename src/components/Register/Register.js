eb8ce00e6ad8ee3efcc57e6e86cc7841d14854cc
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-02-06 13:39:46 +0100

    sync

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ function Register({onRouteChange, loadUser}) {
     const onSubmitReg = () => {
         fetch('https://frozen-shore-44702.herokuapp.com/register', {
             method: 'put',
-            mode: 'cors',
+            //mode: 'cors',
             headers: {
                 'Content-Type': 'application/json',
             },
